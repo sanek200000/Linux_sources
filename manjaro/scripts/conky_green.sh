875261b1bc1a692ca486dbf9f36da4ba557a94c0
Sanek <heist2000@gmail.com>
2023-07-04 23:47:01 +0300

    add scripts

---
+++ manjaro/scripts/conky_green.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+url="https://raw.githubusercontent.com/sanek200000/Linux_sources/main/manjaro/usr/share/conky/conky_green"  # Замените ССЫЛКА_НА_ФАЙЛ на фактическую ссылку на файл
+
+target_dir="/usr/share/conky/"  # Папка, куда нужно сохранить файл
+
+# Создать директорию, если она не существует
+mkdir -p "$target_dir"
+
+# Имя файла, извлекаемое из ссылки
+filename=$(basename "$url")
+
+# Загрузка файла
+curl -Lf -o "$target_dir/$filename" "$url"
+
+echo "Файл успешно сохранен в $target_dir/$filename"
